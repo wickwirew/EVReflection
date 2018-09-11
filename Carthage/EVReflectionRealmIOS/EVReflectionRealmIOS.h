7ba84b9edf8ec4f158f1144cdbd1949e72a62c07
Wesley Wickwire <wickwirew@gmail.com>
2018-09-10 20:06:19 -0400

    added realm carthage target

---
+++ Carthage/EVReflectionRealmIOS/EVReflectionRealmIOS.h
@@ -0,0 +1,19 @@
+//
+//  EVReflectionRealmIOS.h
+//  EVReflectionRealmIOS
+//
+//  Created by Wes Wickwire on 2018-09-10.
+//  Copyright © 2017 wes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EVReflection_Realm_iOS.
+FOUNDATION_EXPORT double EVReflection_Realm_iOSVersionNumber;
+
+//! Project version string for EVReflection_Realm_iOS.
+FOUNDATION_EXPORT const unsigned char EVReflection_Realm_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EVReflection_Realm_iOS/PublicHeader.h>
+
+
